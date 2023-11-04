21a7038ad4a20177cef5b2c6cfc90ef05db357f3
AndresRCA <andresrca97@gmail.com>
2023-11-03 22:08:14 -0400

    pressumably working auth module + debug adjustment

--- backend/app/auth/routes.py
+++ backend/app/auth/routes.py
@@ -1,6 +1,9 @@
+from typing import Optional
 from flask import Blueprint, request, jsonify
 from .models.user import User
+from .models.user_photo import UserPhoto
 from app.extensions import db
+from .auth_service import AuthService
 
 auth_bp = Blueprint("auth", __name__)
 
@@ -15,13 +18,15 @@ def login():
     password = data["password"]
 
     # Check if the user exists in the database
-    user = User.query.filter_by(username=username, password=password).first()
+    user = User.query.filter_by(username=username).first()
     if user:
-        # return only the username
-        print(user)
-        return user
-    else:
-        return jsonify({"error": "Invalid username or password"}), 401
+        return {"id": user.id, "username": user.username, "photo_file_name": user.photo.file_name}
+        # check for password
+        is_valid = AuthService.compare_passwords(user.password, password)
+        if is_valid:
+            return user
+
+    return jsonify({"error": "Invalid username or password"}), 401
 
 
 @auth_bp.route("/logout", methods=["POST"])
@@ -38,22 +43,27 @@ def signup():
         return jsonify({"error": "Invalid data provided"}), 400
 
     username = data["username"]
-    password = data["password"]
+    existing_user = User.query.filter_by(username=username).first()
+    if existing_user:
+        return (
+            jsonify({"error": "Username already exists"}),
+            409,  # send "conflict" status code
+        )
+
+    # get the rest of the data for insertion
+    encrypted_password = AuthService.hash_password(data["password"])
     file_name = data["fileName"]
     data_url = data["photoDataUrl"]
 
-    existing_user = User.query.filter_by(username=username).first()
-    if existing_user:
-        return jsonify({"error": "Username already exists"}), 409 # send "conflict" status code
-
-    new_user = User(username=username, password=password)
+    new_photo = UserPhoto(file_name=file_name, data_url=data_url)
+    new_user = User(username=username, password=encrypted_password, photo=new_photo)
     db.session.add(new_user)
     db.session.commit()
 
-    return jsonify(new_user)
+    return new_user
 
 
-@auth_bp.route("/user-session", methods=["GET"])
+@auth_bp.route("/users/session", methods=["GET"])
 def user_session():
     # Here, you might retrieve user details from the database using the current session/token
     # This logic will depend on how the session/token is managed and associated with users
